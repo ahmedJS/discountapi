15281a5aa2456b32439cd60c3a744930109698c9
ahmedJS <ahmed_alsadi56@outlook.com>
2021-08-25 09:26:58 +0300

    create database class with depedancies folders

---
+++ Src/Mydependencies/Database/DataBase.php
@@ -0,0 +1,23 @@
+<?php
+namespace MyApp\Mydependencies\Database;
+
+use PDO;
+
+class DataBase{
+    private PDO $mycon;
+    private $query;
+    function __construct()
+    {
+        $dsn = "mysql:dbname='discountapi'";
+        $user= "root";
+        $pass= "root";
+        $this->mycon = new PDO($dsn,$user,$pass);
+    }
+
+    function query(\PDOStatement $query,Array $params)
+    {
+        $myquery = $this->mycon->prepare($query);
+        $myquery->execute($params);
+        $this->query = $myquery;
+    }
+}
