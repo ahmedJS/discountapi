f77b3f1d8286420d22548e09065a98ea11a7cd0b
ahmedJS <ahmed_alsadi56@outlook.com>
2021-09-01 22:42:50 +0300

    doing some work on process php

--- Src/Controllers/Process.php
+++ Src/Controllers/Process.php
@@ -4,6 +4,7 @@ namespace Controllers;
 use Psr\Http\Message\ResponseInterface;
 
 class Process extends \Controllers\Header{
+
     private $container;
     function __construct($container)
     {
@@ -15,12 +16,27 @@ class Process extends \Controllers\Header{
      */
     function __invoke($req , $res, $args){
         $token = $args["data"];
-        $jwt_lib = $this->container->JWT;
-
-        if($token) {
-            $jwt_lib::decode($token,);
-        } else {
-            
+        if($token = $this->check_valid_jwt_token($token))
+        {
+            // here processing with database
         }
+    }
+
+    /**
+     * @return array of decoded token on success
+     * @return false when token not valid
+     * @param string $token the token need to validation
+     */
+    function check_valid_jwt_token($token){
+        $jwt_lib = $this->container->JWT;
+        if($token) {
+            // make sure if it valid token or not as well as decode it
+            $details = $jwt_lib::decode($token,$this->get_jwt_key(),"HS256");
+                if($details)
+                {
+                    return $details;
+                }  
+        }
+        return false;
     }
 }
